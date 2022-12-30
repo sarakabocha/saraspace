fecd03ecd53cb1cb9fbdb3e14af16e00f60b6e1f
sara <7968635+sarakabocha@users.noreply.github.com>
2022-12-30 15:21:20 -0800

    get results of round

--- script.js
+++ script.js
@@ -5,6 +5,9 @@
 //If 0.33 < n ≤ 0.66 => paper
 //If 0.66 < n ≤ 1 => scissors
 
+const resultText = document.querySelector('p');
+
+let playerChoice;
 let computerChoice;
 
 function getComputerChoice() {
@@ -13,26 +16,66 @@ function getComputerChoice() {
     console.log(n);
 
     switch(true) {
+
         case n <= 0.33:
-        computerChoice = 'rock'
-        break;
+        return computerChoice = 'rock'
+
 
         case n <= 0.66:
-        computerChoice = 'paper'
-        break;
+        return computerChoice = 'paper'
 
         case n <= 1:
-        computerChoice = 'scissors'
-        break;
+        return computerChoice = 'scissors'
+
     }
     
     console.log(computerChoice);
 }
 
-getComputerChoice();
-
 //Play a single round of rock paper scissors.
 //Take player's selection
+function playRound(playerSelection, computerSelection) {
+    switch(true) {
+
+        case (playerSelection === 'rock' && computerSelection === 'rock'):
+        return 'It\'s a draw.';
+
+        case (playerSelection === 'rock' && computerSelection === 'paper'):
+        return 'Paper wraps up Rock, snatching the victory from the jaws of defeat! Humanity loses this round!';
+
+        case (playerSelection === 'rock' && computerSelection === 'scissors'):
+        return 'Rock smashes the shit out of scissors til it\'s bent out of shape. Humanity prevails this round!';
+
+        case (playerSelection === 'scissors' && computerSelection === 'scissors'):
+        return 'It\'s a draw.';
+        
+        case (playerSelection === 'scissors' && computerSelection === 'rock'):
+        return 'Rock smashes the shit out of scissors til it\'s bent out of shape. Humanity loses this round!';
+
+        case (playerSelection === 'scissors' && computerSelection === 'paper'):
+        return 'Scissors slices through paper and creates confetti. Humanity prevails this round!';
+
+        case (playerSelection === 'paper' && computerSelection === 'paper'):
+        return 'It\'s a draw.';
+
+        case (playerSelection === 'paper' && computerSelection === 'rock'):
+        return 'Paper wraps up Rock, snatching the victory from the jaws of defeat! Humanity prevails this round!';
+
+        case (playerSelection === 'paper' && computerSelection === 'scissors'):
+        return 'Scissors slices through paper and creates confetti. Humanity loses this round!';
+
+    }
+}
+
+const playerSelection = 'rock';
+const computerSelection = getComputerChoice();
+
+console.log(`And the players reveal their moves - Rock...Paper...Scissors!`);
+console.log(`You play ${playerSelection}.`);
+console.log(`The computer plays ${computerSelection}.`);
+
+
+console.log(playRound(playerSelection, computerSelection));
 
 //Take computer's selection
 
