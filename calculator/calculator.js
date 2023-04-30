0d607fb3fb53e6810b8a31919adaab566a202062
sara <7968635+sarakabocha@users.noreply.github.com>
2023-04-29 17:44:59 -0700

    fix logic

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,9 +1,16 @@
+/* TODO
+- Add keyboard support
+- Refactor storeNumber function: The storeNumber function is currently doing too many things. It would be better to break it up into smaller functions that have clear and specific purposes. For example, you could have a function that handles number input, another for operator input, and another for equals input.
+
+*/
+
 const buttons = document.querySelectorAll("button");
 
 const numbers = document.querySelectorAll(".numbers button");
 
 const operators = document.querySelectorAll(".operators button");
-const equals = document.querySelectorAll(".equals button");
+const equals = document.querySelector(".equals button");
+const clear = document.querySelector("#clear");
 
 const display = document.querySelector(".display");
 
@@ -52,33 +59,38 @@ operators.forEach((operator) => {
   });
 });
 
-// Track
-equals.forEach((equal) => {
-  equal.addEventListener("click", () => {
-    storeNumber(equal.id, equal.parentElement.className);
-  });
+equals.addEventListener("click", () => {
+  storeNumber(equals.id, equals.parentElement.className);
 });
 
 let operatorClicked = false;
 let secondNumber = display.textContent;
 let firstNumber;
-let secondOperator;
-let selectedOperator;
-// Store button clicks into variables and call operate function when an operator is clicked
+let storedOperator; //keeps track of the previously selected operator
+let selectedOperator; //keeps track of the currently selected operator
+
+// This function stores button clicks into variables and calls the operate function when an operator is clicked
 function storeNumber(input, className) {
   // If number is selected, build the number as a string
+  clear.addEventListener("click", () => {
+    firstNumber = "";
+    secondNumber = "";
+    storedOperator = "";
+    displayResult(`hello`);
+    console.log(`you pressed clear}`);
+  });
 
-  // store number
+  // Store the number
 
   if (className === "numbers") {
     if (secondNumber === "hello" || operatorClicked) {
       secondNumber = input;
-      // operatorClicked = false;
     } else {
       secondNumber += input;
     }
-
     displayResult(secondNumber);
+
+    // If an operator was pressed, run the code
   } else if (className === "operators") {
     if (input === "+") {
       selectedOperator = add;
@@ -89,37 +101,52 @@ function storeNumber(input, className) {
     } else if (input === "/") {
       selectedOperator = divide;
     }
-    // operatorClicked = true;
 
-    if (firstNumber && secondOperator && secondNumber) {
-      secondNumber = operate(secondOperator, +firstNumber, +secondNumber);
+    // We run a check to see if an operator was previously stored; if so, then the previous operation is run and its result is displayed. The currently selected operator is stored in selectedOperator.
+    if (firstNumber && storedOperator && secondNumber) {
+      secondNumber = operate(storedOperator, +firstNumber, +secondNumber);
       displayResult(secondNumber);
-
-      // operatorClicked = false;
     }
 
-    // Whenever an operator is clicked, we pass the value from secondNumber to firstNumber, and store the selected operation until the next input is selected
+    // Whenever an operator is clicked, we transfer the value from secondNumber to firstNumber, and store the selected operator until the next input is selected
     firstNumber = secondNumber;
     secondNumber = "";
-    secondOperator = selectedOperator;
+    storedOperator = selectedOperator;
   } else if (className === "equals") {
-    console.log(`You pressed equals. Storing a value of ${firstNumber}`);
-    secondNumber = firstNumber;
-    selectedOperator = "";
+    // Whenever equals is clicked, the operation is performed with the stored operator (same as above), but the first number and stored operator are reset.
+    if (firstNumber && storedOperator && secondNumber) {
+      secondNumber = operate(storedOperator, +firstNumber, +secondNumber);
+      displayResult(secondNumber);
+
+      // The firstNumber and secondOperator variables are reset to empty strings after the calculation is performed.
+      firstNumber = "";
+      storedOperator = "";
+    }
+
+    console.log(`your new secondNumber is ${secondNumber}`);
+    console.log(`your new firstNumber is ${firstNumber}`);
+    console.log(`your next operation is ${storedOperator}`);
+    return secondNumber;
+
+    //   // Clear all
+    // } else if (className === "clear") {
+    //   firstNumber = "";
+    //   secondNumber = "";
+    //   storedOperator = "";
+    //   displayResult(`hello`);
   }
 
-  console.log(`your new secondNumber is ${secondNumber}`);
-  console.log(`your new firstNumber is ${firstNumber}`);
-  console.log(`your next operation is ${secondOperator}`);
-  return secondNumber;
+  // display values of the pressed buttons
+  function displayResult(text) {
+    return (display.textContent = `${text}`);
+  }
 }
 
-// display values of the pressed buttons
-function displayResult(text) {
-  return (display.textContent = `${text}`);
-}
+/* Explanation
 
-// If operator is selected, store the number as variable a, and build the next number
+secondOperator and selectedOperator are used to keep track of the operator that the user has clicked.
 
-// If operator is selected, run the operation with initial value (variable a) set by conditional:
-// If add/subtract, initial value = 0; if multiple/divide, initial value = 1.
+selectedOperator is assigned the value of the operator that is clicked by the user in the storeNumber function when the className is "operators". This is done in order to keep track of the operator that has been selected by the user, and it is used later in the code when the equal button is pressed.
+
+secondOperator is used to keep track of the operator that was previously selected by the user. It is used in the storeNumber function when a new operator is clicked. If the user has already selected a first number, a second number and an operator, and then selects a new operator, secondOperator will be used to perform the operation using the previous operator and the previous two numbers.
+*/
